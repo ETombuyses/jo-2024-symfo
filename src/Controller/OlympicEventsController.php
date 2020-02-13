71f450b1b8322a5c99f2b50662150e3313896a18
ETombuyses <e.tombuyses@gmail.com>
2020-02-13 12:08:48 +0100

    fix anotation

--- src/Controller/OlympicEventsController.php
+++ src/Controller/OlympicEventsController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Repository\OlympicEventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OlympicEventsController extends AbstractController
 {
     /**
      * @Route("/olympiceventsbydate", name="olympic_events")
+     * @param OlympicEventRepository $repository
+     * @return JsonResponse
      */
     public function index(OlympicEventRepository $repository)
     {
